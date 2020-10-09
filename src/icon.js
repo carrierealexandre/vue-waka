9fac5a85f1c8f5924e500f75d7ae00f66b8e4e9f
Alexandre Carriere <carriere.alexandre@live.ca>
2020-10-09 01:25:47 -0400

    added icons

--- src/icon.js
+++ src/icon.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faExclamationTriangle, faChartLine, faChevronRight, faChevronLeft, faEllipsisV, faRedoAlt, faPlusSquare, faPlus,faEye,faTrashAlt,faEdit ,faUserSecret, faSpinner, faTree, faSearch,faQuestionCircle, faShoppingCart, faUserCircle, faCaretDown, faPlaneArrival, faPlane, faArrowAltCircleRight,faDollarSign, faCartPlus, faSnowflake, faCircleNotch, faSignInAlt,} from '@fortawesome/free-solid-svg-icons'
+import { faTimesCircle, faCheckCircle, faExclamationTriangle, faChartLine, faChevronRight, faChevronLeft, faEllipsisV, faRedoAlt, faPlusSquare, faPlus,faEye,faTrashAlt,faEdit ,faUserSecret, faSpinner, faTree, faSearch,faQuestionCircle, faShoppingCart, faUserCircle, faCaretDown, faPlaneArrival, faPlane, faArrowAltCircleRight,faDollarSign, faCartPlus, faSnowflake, faCircleNotch, faSignInAlt,} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add( faExclamationTriangle, faChartLine,faChevronRight, faChevronLeft,faEllipsisV, faRedoAlt, faPlusSquare, faPlus,faEye,faTrashAlt,faEdit,faUserSecret, faSpinner, faTree, faSearch, faQuestionCircle, faShoppingCart, faUserCircle, faCaretDown, faPlaneArrival, faPlane, faArrowAltCircleRight,faDollarSign, faCartPlus, faSnowflake, faCircleNotch, faSignInAlt)
+library.add( faTimesCircle, faCheckCircle, faExclamationTriangle, faChartLine,faChevronRight, faChevronLeft,faEllipsisV, faRedoAlt, faPlusSquare, faPlus,faEye,faTrashAlt,faEdit,faUserSecret, faSpinner, faTree, faSearch, faQuestionCircle, faShoppingCart, faUserCircle, faCaretDown, faPlaneArrival, faPlane, faArrowAltCircleRight,faDollarSign, faCartPlus, faSnowflake, faCircleNotch, faSignInAlt)
 
 Vue.component('fa-icon', FontAwesomeIcon)
